2f9c25cc47e9adb91fb0fed9d90bb7a01e3f061f
Vytautas Karpavicius <vytautas@uber.com>
2021-05-31 17:32:38 +0300

    Added helper for time/duration conversion

---
+++ internal/api/helpers.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2017-2021 Uber Technologies Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package api
+
+import (
+	"time"
+
+	gogo "github.com/gogo/protobuf/types"
+)
+
+func TimestampFromProto(t *gogo.Timestamp) time.Time {
+	timestamp, err := gogo.TimestampFromProto(t)
+	if err != nil {
+		panic(err)
+	}
+	return timestamp
+}
+
+func TimestampToProto(t time.Time) *gogo.Timestamp {
+	timestamp, err := gogo.TimestampProto(t)
+	if err != nil {
+		panic(err)
+	}
+	return timestamp
+}
+
+func DurationFromProto(d *gogo.Duration) time.Duration {
+	duration, err := gogo.DurationFromProto(d)
+	if err != nil {
+		panic(err)
+	}
+	return duration
+}
+
+func DurationToProto(d time.Duration) *gogo.Duration {
+	return gogo.DurationProto(d)
+}
